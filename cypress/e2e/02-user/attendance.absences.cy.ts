59aa79d884996b5702b0438e6e469ca81628e0d4
Dan Gowans <dangowans@users.noreply.github.com>
2023-06-21 15:20:49 -0400

    add absence record test

--- cypress/e2e/02-user/attendance.absences.cy.ts
+++ cypress/e2e/02-user/attendance.absences.cy.ts
@@ -1,3 +1,6 @@
+/* eslint-disable promise/always-return */
+/* eslint-disable promise/catch-or-return */
+
 import { testUser } from '../../../test/_globals.js'
 import type { ConfigTemporaryUserCredentials } from '../../../types/configTypes'
 import { logout, login } from '../../support/index.js'
@@ -7,14 +10,63 @@ describe('Attendance - Absences', () => {
     logout()
     login(testUser as ConfigTemporaryUserCredentials)
     cy.visit('/attendance')
+    cy.location('pathname').should('equal', '/attendance')
+    cy.get('.menu-list a[href="#tab--absences"]')
+      .click()
+      .should('have.class', 'is-active')
   })
 
   afterEach(logout)
 
   it('Has no detectable accessibility issues', () => {
-    cy.visit('/attendance')
-    cy.location('pathname').should('equal', '/attendance')
     cy.injectAxe()
     cy.checkA11y()
+  })
+
+  it('Adds an absence request', () => {
+    // Open the add modal
+
+    cy.get('[data-cy="add-absence"]').click()
+
+    cy.get('.modal').should('exist')
+    cy.get('html').should('have.class', 'is-clipped')
+
+    cy.injectAxe()
+    cy.checkA11y()
+
+    cy.get('#container--employees a[data-employee-number]')
+      .first()
+      .then(($element) => {
+        const employeeNumber = $element.data('employee-number')
+
+        cy.get('.modal input[name="employeeNumber"]')
+          .should('have.focus')
+          .clear()
+          .type(employeeNumber)
+
+        cy.wait(250)
+
+        cy.get('.modal input[name="employeeName"]')
+          .should('have.prop', 'value')
+          .should('not.be.empty')
+
+        cy.get('.modal input[name="callInType"][value="absence"]').click({
+          force: true
+        })
+
+        cy.get(
+          '.modal fieldset[data-call-in-type="absence"] select[name="absenceTypeKey"]'
+        ).select(1)
+
+        cy.get('.modal textarea[name="recordComment"]')
+          .clear()
+          .type('Test Comment')
+      })
+
+    cy.get('.modal form').submit()
+
+    // Ensure the modal closed
+
+    cy.get('.modal').should('not.exist')
   })
 })
