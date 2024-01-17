9bfd370f248209e4fb60ecb408539d9acb35e7ac
Dan Gowans <dangowans@users.noreply.github.com>
2024-01-17 10:55:42 -0500

    toggle showing row numbers

--- public-typescript/print.callOutList.js
+++ public-typescript/print.callOutList.js
@@ -1,16 +1,32 @@
 ;
 (() => {
+    function toggleColumn(inputElement, cellElements) {
+        for (const cellElement of cellElements) {
+            if (inputElement.checked) {
+                cellElement.classList.remove('is-hidden');
+            }
+            else {
+                cellElement.classList.add('is-hidden');
+            }
+        }
+    }
+    /*
+     * Row Number Toggle
+     */
+    const rowNumberToggleCheckboxElement = document.querySelector('#option--showRowNumber');
+    const rowNumberCellElements = document.querySelectorAll('.column--rowNumber');
+    function toggleShowRowNumber() {
+        toggleColumn(rowNumberToggleCheckboxElement, rowNumberCellElements);
+    }
+    rowNumberToggleCheckboxElement.addEventListener('click', toggleShowRowNumber);
+    toggleShowRowNumber();
+    /*
+     * Sort Key Toggle
+     */
     const sortKeyToggleCheckboxElement = document.querySelector('#option--showSortKey');
     const sortKeyCellElements = document.querySelectorAll('.column--sortKey');
     function toggleShowSortKey() {
-        for (const sortKeyCellElement of sortKeyCellElements) {
-            if (sortKeyToggleCheckboxElement.checked) {
-                sortKeyCellElement.classList.remove('is-hidden');
-            }
-            else {
-                sortKeyCellElement.classList.add('is-hidden');
-            }
-        }
+        toggleColumn(sortKeyToggleCheckboxElement, sortKeyCellElements);
     }
     sortKeyToggleCheckboxElement.addEventListener('click', toggleShowSortKey);
     toggleShowSortKey();
--- public-typescript/print.callOutList.ts
+++ public-typescript/print.callOutList.ts
@@ -1,17 +1,46 @@
 ;(() => {
+  function toggleColumn(
+    inputElement: HTMLInputElement,
+    cellElements: NodeListOf<Element>
+  ): void {
+    for (const cellElement of cellElements) {
+      if (inputElement.checked) {
+        cellElement.classList.remove('is-hidden')
+      } else {
+        cellElement.classList.add('is-hidden')
+      }
+    }
+  }
+
+  /*
+   * Row Number Toggle
+   */
+
+  const rowNumberToggleCheckboxElement = document.querySelector(
+    '#option--showRowNumber'
+  ) as HTMLInputElement
+
+  const rowNumberCellElements = document.querySelectorAll('.column--rowNumber')
+
+  function toggleShowRowNumber(): void {
+    toggleColumn(rowNumberToggleCheckboxElement, rowNumberCellElements)
+  }
+
+  rowNumberToggleCheckboxElement.addEventListener('click', toggleShowRowNumber)
+  toggleShowRowNumber()
+
+  /*
+   * Sort Key Toggle
+   */
+
   const sortKeyToggleCheckboxElement = document.querySelector(
     '#option--showSortKey'
   ) as HTMLInputElement
+
   const sortKeyCellElements = document.querySelectorAll('.column--sortKey')
 
   function toggleShowSortKey(): void {
-    for (const sortKeyCellElement of sortKeyCellElements) {
-      if (sortKeyToggleCheckboxElement.checked) {
-        sortKeyCellElement.classList.remove('is-hidden')
-      } else {
-        sortKeyCellElement.classList.add('is-hidden')
-      }
-    }
+    toggleColumn(sortKeyToggleCheckboxElement, sortKeyCellElements)
   }
 
   sortKeyToggleCheckboxElement.addEventListener('click', toggleShowSortKey)
